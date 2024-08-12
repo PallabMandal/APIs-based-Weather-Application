d699a41b9ead1bf729d39df52ccd2165165a6604
Pallab Mandal <160285856+PallabMandal@users.noreply.github.com>
2024-08-13 02:47:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Weather from './components/currentLocation';
+
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <Weather />
+      </div>
+      <div className="footer-info">
+        <a href="/">
+          Download Source Code
+        </a>{" "}
+        | Developed by{" "}
+        <a target="_blank" href="/">
+          Pallab Mandal
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="/">
+          REACT JS
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "f8febebfc211451597a9ee9416dd4e06",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
